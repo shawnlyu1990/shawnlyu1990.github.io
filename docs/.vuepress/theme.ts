3f94776f7c9fb5674ad9e02000364b43f5a52946
Shawn Lyu <shawnlyu1990@gmail.com>
2024-08-06 14:27:29 +0800

    更新配置文件

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -4,10 +4,10 @@ import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://mister-hope.github.io",
+  hostname: "https://shawnlyu1990.github.io",
 
   author: {
-    name: "Mr.Hope",
+    name: "Shawn Lyu",
     url: "https://mister-hope.com",
   },
 
@@ -21,37 +21,37 @@ export default hopeTheme({
 
   blog: {
     medias: {
-      Baidu: "https://example.com",
-      BiliBili: "https://example.com",
-      Bitbucket: "https://example.com",
-      Dingding: "https://example.com",
-      Discord: "https://example.com",
-      Dribbble: "https://example.com",
-      Email: "mailto:info@example.com",
-      Evernote: "https://example.com",
-      Facebook: "https://example.com",
-      Flipboard: "https://example.com",
-      Gitee: "https://example.com",
-      GitHub: "https://example.com",
-      Gitlab: "https://example.com",
-      Gmail: "mailto:info@example.com",
-      Instagram: "https://example.com",
-      Lark: "https://example.com",
-      Lines: "https://example.com",
-      Linkedin: "https://example.com",
-      Pinterest: "https://example.com",
-      Pocket: "https://example.com",
-      QQ: "https://example.com",
-      Qzone: "https://example.com",
-      Reddit: "https://example.com",
-      Rss: "https://example.com",
-      Steam: "https://example.com",
-      Twitter: "https://example.com",
-      Wechat: "https://example.com",
-      Weibo: "https://example.com",
-      Whatsapp: "https://example.com",
-      Youtube: "https://example.com",
-      Zhihu: "https://example.com",
+      //Baidu: "https://example.com",
+      //BiliBili: "https://example.com",
+      //Bitbucket: "https://example.com",
+      //Dingding: "https://example.com",
+      //Discord: "https://example.com",
+      //Dribbble: "https://example.com",
+      //Email: "mailto:info@example.com",
+      //Evernote: "https://example.com",
+      //Facebook: "https://example.com",
+      //Flipboard: "https://example.com",
+      //Gitee: "https://example.com",
+      GitHub: "https://github.com/shawnlyu1990/shawnlyu1990.github.io.git",
+      //Gitlab: "https://example.com",
+      Gmail: "mailto:shawnlyu1990@gmail.com",
+      //Instagram: "https://example.com",
+      //Lark: "https://example.com",
+      //Lines: "https://example.com",
+      //Linkedin: "https://example.com",
+      //Pinterest: "https://example.com",
+      //Pocket: "https://example.com",
+      //QQ: "https://example.com",
+      //Qzone: "https://example.com",
+      //Reddit: "https://example.com",
+      //Rss: "https://example.com",
+      //Steam: "https://example.com",
+      //Twitter: "https://example.com",
+      //Wechat: "https://example.com",
+      //Weibo: "https://example.com",
+      //Whatsapp: "https://example.com",
+      //Youtube: "https://example.com",
+      //Zhihu: "https://example.com",
       VuePressThemeHope: {
         icon: "https://theme-hope-assets.vuejs.press/logo.svg",
         link: "https://theme-hope.vuejs.press",
@@ -61,30 +61,6 @@ export default hopeTheme({
 
   locales: {
     "/": {
-      // navbar
-      navbar: enNavbar,
-
-      // sidebar
-      sidebar: enSidebar,
-
-      footer: "Default footer",
-
-      displayFooter: true,
-
-      blog: {
-        description: "A FrontEnd programmer",
-        intro: "/intro.html",
-      },
-
-      metaLocales: {
-        editLink: "Edit this page on GitHub",
-      },
-    },
-
-    /**
-     * Chinese locale config
-     */
-    "/zh/": {
       // navbar
       navbar: zhNavbar,
 
@@ -97,10 +73,9 @@ export default hopeTheme({
 
       blog: {
         description: "一个前端开发者",
-        intro: "/zh/intro.html",
+        intro: "/intro.html",
       },
 
-      // page meta
       metaLocales: {
         editLink: "在 GitHub 上编辑此页",
       },
@@ -129,7 +104,20 @@ export default hopeTheme({
     // },
 
     components: {
-      components: ["Badge", "VPCard"],
+      components: [
+        "ArtPlayer",
+        "Badge",
+        "BiliBili",
+        "CodePen",
+        "PDF",
+        "Share",
+        "SiteInfo",
+        "StackBlitz",
+        "VPBanner",
+        "VPCard",
+        "VidStack",
+        "XiGua",
+      ],
     },
 
     // These features are enabled for demo, only preserve features you need here
